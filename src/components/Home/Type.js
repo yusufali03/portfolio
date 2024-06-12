c9f2c5dab33d147479ea5702847556078d20a7c3
yusufali03 <115856762+yusufali03@users.noreply.github.com>
2024-06-12 09:16:59 +0500

    This is portfolio page

--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -5,12 +5,7 @@ function Type() {
   return (
     <Typewriter
       options={{
-        strings: [
-          "Software Developer",
-          "Freelancer",
-          "MERN Stack Developer",
-          "Open Source Contributor",
-        ],
+        strings: ["Frontend Developer", "Freelancer", "AI Learner"],
         autoStart: true,
         loop: true,
         deleteSpeed: 50,
